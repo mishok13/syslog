71a64f678606505bb716b96db70126f16e9deb60
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-04-20 12:26:27 +0200

    Kicking off the project

---
+++ README.md
@@ -0,0 +1,9 @@
+# me.mishok13/syslog
+
+Pure Clojure implementation of RFC 5424 (syslog protocol).
+
+## License
+
+Copyright © 2015 Andrii V. Mishkovskyi
+
+Distributed under the MIT license.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject me.mishok13/syslog "0.1.0-SNAPSHOT"
+  :description "Implementation of RFC 5424 aka syslog protocol"
+  :url "http://mishok13.me/libs/syslog"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
