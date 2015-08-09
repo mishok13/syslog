aa311bbb2d57556024c9c4dbae4e8327f245c9a7
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-08-09 12:11:54 +0200

    Initial implementation complete
    This implementation has a simple formatting, pretty inefficient byte
    array compilation routine and an extremely simple test.

--- src/me/mishok13/syslog.clj
+++ src/me/mishok13/syslog.clj
@@ -1,4 +1,7 @@
-(ns me.mishok13.syslog)
+(ns me.mishok13.syslog
+  (:require [clj-time.core :as time.core]
+            [clj-time.format :as time.format])
+  (:import [java.io ByteArrayOutputStream]))
 
 (def ^:private separator \space)
 (def ^:private print-us-ascii (set (map char (range 33 127))))
@@ -6,5 +9,101 @@
 (def ^:private bom (map char [0xEF 0xBB 0xBF]))
 (def ^:private escape-characters [\" \\ \]])
 
-(defrecord Header [priority version timestamp hostname app-name proc-id msg-id])
+(defn- printable-ascii?
+  [bytes]
+  (every? (fn [b] (<= 33 b 126)) (vec bytes)))
+
+(defprotocol ISyslogFormattable
+  (show [this] [this writer]
+    "Format the part of syslog according to RFC 5424"))
+
+(def ^:private timestamp-formatter
+  (time.format/formatter "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
+
+(defrecord Header [priority version timestamp hostname app-name proc-id msg-id]
+  ISyslogFormattable
+  (show [this writer]
+    (let [pri-value (.getBytes (str "<" priority ">") "ASCII")
+          version (.getBytes (str version) "ASCII")
+          timestamp (.getBytes (time.format/unparse timestamp-formatter timestamp) "ASCII")
+          hostname (.getBytes (str hostname) "ASCII")
+          app-name (.getBytes (str app-name) "ASCII")
+          proc-id (.getBytes (str proc-id) "ASCII")
+          msg-id (.getBytes (str msg-id) "ASCII")]
+      (.write writer pri-value 0 (count pri-value))
+      (.write writer version 0 (count version))
+      (.write writer (int separator))
+      (.write writer timestamp 0 (count timestamp))
+      (.write writer (int separator))
+      (.write writer hostname 0 (count hostname))
+      (.write writer (int separator))
+      (.write writer app-name 0 (count app-name))
+      (.write writer (int separator))
+      (.write writer proc-id 0 (count proc-id))
+      (.write writer (int separator))
+      (.write writer msg-id 0 (count msg-id))
+      writer)))
+
+(defrecord StructuredData [elements]
+  ISyslogFormattable
+  (show [this writer]
+    (cond
+      (seq elements) writer
+      :else (.write writer (int \-)))))
+
+(extend-protocol ISyslogFormattable
+  java.lang.String
+  (show [this writer]
+    (.write writer (- 239 128))
+    (.write writer (- 187 128))
+    (.write writer (- 191 129))
+    (let [bytes (.getBytes this "UTF-8")]
+      (.write writer bytes 0 (count bytes)))))
+
 (defrecord SyslogMessage [header structured-data message])
+
+(defn- valid-ascii?
+  [s min-length max-length]
+  (and (string? s)
+       (let [bytes (.getBytes s "ASCII")]
+         (and
+          (printable-ascii? bytes)
+          (<= min-length (count bytes) max-length)))))
+
+(defn nilvalue
+  [s]
+  (cond
+    (nil? s) "-"
+    :else (str s)))
+
+(defn make-message
+  [& {:keys [facility severity version timestamp hostname app-name proc-id msg-id data message]}]
+  (assert (and facility (<= 0 facility 23)))
+  (assert (and severity (<= 0 severity 7)))
+  (assert (and version (<= 0 version 99)))
+  (assert (or (nil? hostname) (valid-ascii? hostname 1 255)))
+  (assert (or (nil? app-name) (valid-ascii? app-name 1 48)))
+  (assert (or (nil? proc-id) (valid-ascii? proc-id 1 128)))
+  (let [priority (+ severity (* 8 facility))
+        header (Header. (nilvalue priority)
+                        (nilvalue version)
+                        timestamp
+                        (nilvalue hostname)
+                        (nilvalue app-name)
+                        (nilvalue proc-id)
+                        (nilvalue msg-id))
+        structured-data (StructuredData. data)
+        message message]
+    (SyslogMessage. header structured-data message)))
+
+(defn format-message
+  "Format message to a byte representation"
+  [message]
+  (let [writer (ByteArrayOutputStream.)]
+    (show (:header message) writer)
+    (.write writer (int separator))
+    (show (:structured-data message) writer)
+    (when (:message message)
+      (.write writer (int separator))
+      (show (:message message) writer))
+    (.toByteArray writer)))
---
+++ test/me/mishok13/syslog_test.clj
@@ -0,0 +1,31 @@
+(ns me.mishok13.syslog-test
+  (:require [me.mishok13.syslog :refer :all]
+            [midje.sweet :refer :all]
+            [clj-time.core :refer [date-time]]))
+
+;; The following test is using examples from RFC 5424, section 6.5;
+;; see more here https://tools.ietf.org/html/rfc5424#section-6.5
+(fact
+ "Syslog message is formatted correctly"
+ ;;
+ (vec (format-message (make-message :version 1
+                                    :facility 4
+                                    :severity 2
+                                    :timestamp (date-time 2003 10 11 22 14 15 3)
+                                    :hostname "mymachine.example.com"
+                                    :app-name "su"
+                                    :proc-id nil
+                                    :msg-id "ID47"
+                                    :message "'su root' failed for lonvick on /dev/pts/8")))
+ =>
+ ;; This is generated from the following message:
+ ;; "<34>1 2003-10-11T22:14:15.003Z mymachine.example.com su - ID47 - BOM'su root' failed for lonvick on /dev/pts/8"
+ ;; where BOM was replaced with an actual BOM
+ [60 51 52 62 49 32 50 48 48 51 45 49 48 45 49 49 84 50 50 58 49 52 58 49 53 46
+  48 48 51 90 32 109 121 109 97 99 104 105 110 101 46 101 120 97 109 112 108 101
+  46 99 111 109 32 115 117 32 45 32 73 68 52 55 32 45 32 111 59 62 39 115 117
+  32 114 111 111 116 39 32 102 97 105 108 101 100 32 102 111 114 32 108 111 110
+  118 105 99 107 32 111 110 32 47 100 101 118 47 112 116 115 47 56]
+ ;; (format-message (make-message :version 1)) =>
+ ;; "<165>1 2003-08-24T05:14:15.000003-07:00 192.0.2.1 myproc 8710 - - %% It's time to make the do-nuts."
+ )
