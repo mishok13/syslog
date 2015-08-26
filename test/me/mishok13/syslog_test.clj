06dd98cc51f32adb606e1f4523acd4a5fd35dbab
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-08-26 14:24:38 +0200

    Finalized test #2 for RFC 5424 formatting

--- test/me/mishok13/syslog_test.clj
+++ test/me/mishok13/syslog_test.clj
@@ -35,12 +35,15 @@
   32 114 111 111 116 39 32 102 97 105 108 101 100 32 102 111 114 32 108 111 110
   118 105 99 107 32 111 110 32 47 100 101 118 47 112 116 115 47 56]
  ;; Note that this uses a modified version of the test, since this
- ;; library does not support microsecond precision. The message is:
+ ;; library does not support microsecond precision. The original
+ ;; message is:
  ;; "<165>1 2003-08-24T05:14:15.000003-07:00 192.0.2.1 myproc 8710 - - %% It's time to make the do-nuts."
+ ;; Message as used for testing is:
+ ;; "<165>1 2003-08-24T05:14:15.003-07:00 192.0.2.1 myproc 8710 - - %% It's time to make the do-nuts."
+ ;; Also note that this version explicitly drops BOM before MSG part
  [:version 1 :facility 20 :severity 5
   :timestamp (DateTime. 2003 8 24 5 14 15 3 (DateTimeZone/forOffsetHours -7))
+  :force-utc? false
   :hostname "192.0.2.1" :app-name "myproc" :proc-id "8710"
-  :message "%% It's time to make the do-nuts."]
- []
-
- )
+  :message (.getBytes "%% It's time to make the do-nuts." "ASCII") :bom? false]
+ [60 49 54 53 62 49 32 50 48 48 51 45 48 56 45 50 52 84 48 53 58 49 52 58 49 53 46 48 48 51 45 48 55 58 48 48 32 49 57 50 46 48 46 50 46 49 32 109 121 112 114 111 99 32 56 55 49 48 32 45 32 45 32 37 37 32 73 116 39 115 32 116 105 109 101 32 116 111 32 109 97 107 101 32 116 104 101 32 100 111 45 110 117 116 115 46])
