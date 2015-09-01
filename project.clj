335bb80f85bbc0d329ce2cc343d40915878b943a
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-09-01 14:29:37 +0200

    Correct dependency on Clojure 1.8

--- project.clj
+++ project.clj
@@ -9,4 +9,4 @@
                    :global-vars {*warn-on-reflection* true}}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-SNAPSHOT"]]}})
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-alpha4"]]}})
